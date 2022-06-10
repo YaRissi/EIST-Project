b855acade14b8d9ec64c6cf572c51f1f6beafe9b
hanng <ge63guf@mytum.de>
2022-06-10 13:34:16 +0200

    ReservationSystemTest zurückgesetzt und Package userInterface erstellt

--- src/test/java/reservationsystem/testing/ReservationSystemTest.java
+++ src/test/java/reservationsystem/testing/ReservationSystemTest.java
@@ -1,18 +1,9 @@
 package reservationsystem.testing;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
-public class ReservationSystemTest extends Application {
+public class ReservationSystemTest{
 
     public static void main(String[] args) {
-        launch();
+
     }
 
-    @Override
-    public void start(Stage primaryStage) throws IOException {
-        // Hier kommt der ganze GUi Stuff rein
-    }
 }
