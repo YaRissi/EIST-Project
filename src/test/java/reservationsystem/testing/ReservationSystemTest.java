913ed13a74be82802b5a0cd5f413e9c4cd6eb4a4
hanng <ge63guf@mytum.de>
2022-06-10 13:09:18 +0200

    build.gradle JavaFX eingefügt ReservationSystemTest von Application erben lassen

--- src/test/java/reservationsystem/testing/ReservationSystemTest.java
+++ src/test/java/reservationsystem/testing/ReservationSystemTest.java
@@ -1,6 +1,18 @@
 package reservationsystem.testing;
 
-public class ReservationSystemTest {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ReservationSystemTest extends Application {
+
     public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        // Hier kommt der ganze GUi Stuff rein
     }
 }
