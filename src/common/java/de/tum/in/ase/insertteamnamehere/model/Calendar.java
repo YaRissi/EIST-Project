6b99b11661398281baca66cb8c8740ea9acf3efa
David Steinberg <david.steinberg17@gmail.com>
2022-06-18 03:31:22 +0200

    changes

--- src/common/java/de/tum/in/ase/insertteamnamehere/model/Calendar.java
+++ src/common/java/de/tum/in/ase/insertteamnamehere/model/Calendar.java
@@ -1,4 +1,4 @@
-package model;
+package de.tum.in.ase.insertteamnamehere.model;
 import java.util.ArrayList;
 import java.util.List;
 
--- src/common/java/de/tum/in/ase/insertteamnamehere/model/CalendarField.java
+++ src/common/java/de/tum/in/ase/insertteamnamehere/model/CalendarField.java
@@ -1,4 +1,4 @@
-package model;
+package de.tum.in.ase.insertteamnamehere.model;
 
 import java.util.GregorianCalendar;
 
