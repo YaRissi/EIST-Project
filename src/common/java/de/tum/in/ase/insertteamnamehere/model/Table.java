67d3730798f75c99825f68df3103f26c8340053e
ge59buc <ge59buc@mytum.de>
2022-07-20 12:18:07 +0200

    <added method in table to see available slots (difference between opening times and already reserved slots) + restaurant attribute>

--- src/common/java/de/tum/in/ase/insertteamnamehere/model/Table.java
+++ src/common/java/de/tum/in/ase/insertteamnamehere/model/Table.java
@@ -1,10 +1,15 @@
 package de.tum.in.ase.insertteamnamehere.model;
 
-import java.util.UUID;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Table {
     private boolean reserved;
     private final String tableID;
+
+    private Restaurant restaurant;
 
     private final int maxNumberOfPeople;
 
@@ -29,6 +34,55 @@ public class Table {
 
     public String getTableID() {
         return tableID;
+    }
+
+    public boolean addReservation(Reservation reservation) {
+        return restaurant.getReservations().add(reservation);
+    }
+    public boolean deleteReservation(Reservation reservation) {
+        return restaurant.getReservations().remove(reservation);
+    }
+
+    public List<Reservation> getReservationsForTable() {
+        return restaurant.getReservations().stream().filter(r -> r.getTable().getTableID().equals(this.getTableID())).collect(Collectors.toList());
+    }
+
+    public List<TimeSlot> getAvailableTimeSlots(LocalDate date) {
+        List<TimeSlot> availableTimes = restaurant.getOpeningTimes().get(date.getDayOfWeek().getValue() - 1);
+        List<TimeSlot> reservedTimes = getReservationsForTable().stream().map(r -> r.getTimeslot()).toList();
+
+        for(int i = 0; i < reservedTimes.size(); i++) {
+
+            TimeSlot reserved = reservedTimes.get(i);
+            TimeSlot openingTimeSlot = reserved.isInBounds(availableTimes);
+
+            if(openingTimeSlot != null) {
+
+                LocalTime availableStart = openingTimeSlot.getOpen();
+                LocalTime reservedStart = reserved.getOpen();
+
+                if(reservedStart.compareTo(availableStart) == 0) {
+
+                    TimeSlot one = new TimeSlot(reservedStart, reserved.getClosed());
+                    TimeSlot two = new TimeSlot(reserved.getClosed(), openingTimeSlot.getClosed());
+
+                    availableTimes.remove(openingTimeSlot);
+                    availableTimes.add(one);
+                    availableTimes.add(two);
+
+                } else if(reservedStart.compareTo(availableStart) > 0) {
+                    TimeSlot one = new TimeSlot(availableStart, reservedStart);
+                    TimeSlot two = new TimeSlot(reservedStart, reserved.getClosed());
+                    TimeSlot three = new TimeSlot(reserved.getClosed(), openingTimeSlot.getClosed());
+
+                    availableTimes.remove(openingTimeSlot);
+                    availableTimes.add(one);
+                    availableTimes.add(two);
+                    availableTimes.add(three);
+                }
+            }
+        }
+        return availableTimes;
     }
 
 }
